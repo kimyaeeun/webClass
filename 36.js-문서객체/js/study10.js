7d55183608f0455ab7ca78b7ee912844c5dfa6b2
kimyaeeun <kimye1104@naver.com>
2023-05-25 18:12:33 +0900

    flexible

--- 36.js-문서객체/js/study10.js
+++ 36.js-문서객체/js/study10.js
@@ -1,12 +1,44 @@
 window.onload = function(){
-    let slider1 = new Swiper(".station1",{
-        loop: true,
-    })
-
-    let slider2 = new Swiper(".station2",{
+    let station1 = new Swiper(".station1",{
+        loop:true,
+        direction:'horizontal',
+        // slidesPerView:2, //슬라이드 개수의 반만큼 최대 적용 가능
+        // spaceBetween:40,
+        pagination: {
+              el: '.swiper-pagination',
+              type: 'progressbar',
+              clickable : true
+            },
         navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
+                nextEl: ".csNextBtn",
+                prevEl: ".csPrevBtn",
+              },
+        autoplay:{
+            delay:1000,
+            // pauseOnMouseEnter:true
+        },
+        speed:1000,
+        breakpoints:{
+            320:{
+                slidesPerView:1,
+                spaceBetween:0
+            },
+            768:{
+                slidesPerView:2,
+                spaceBetween:10
+            },
+            1024:{
+                slidesPerView:3,
+                spaceBetween:20
+            }
         }
-    })
+          })
+
+          document.querySelector(".station1").addEventListener("mouseover",function(){
+            station1.autoplay.stop()
+          })
+          document.querySelector(".station1").addEventListener("mouseout",function(){
+            station1.autoplay.start()
+          })
+
 }
